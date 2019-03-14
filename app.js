ff7dfa83e355569f08e130a9d2d4d0f14f1098c5
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-03-14 10:17:53 +0530

    Added mock post route

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -10,7 +13,12 @@ app.get("/", function(req, res) {
 
 app.get("/theworldthroughmylenses", function(req,res) {
     res.render("index.ejs");
-})
+});
+
+app.post("/theworldthroughmylenses/addpost", function(req, res) {
+    console.log(req.body);
+    res.redirect("/");
+});
 
 app.listen(4200, function() {
     console.log("Server is running on PORT 4200!");
