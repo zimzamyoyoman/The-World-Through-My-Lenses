2a76faa87b639ca24b676093e5d5718d7e97736c
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-03-14 10:03:22 +0530

    Added public and partials directory and files

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
     res.redirect("/theworldthroughmylenses");
 });
