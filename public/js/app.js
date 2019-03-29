dcdf0c9d173d5d0f3c924e19eb0f4ff6c7374f91
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-03-29 16:59:26 +0530

    Changed background and heading color

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var container = document.querySelector('#masonry');
 var masonry = new Masonry(container, {
-  columnWidth: 50,
+  columnWidth: 80,
   itemSelector: '.item',
   horizontalOrder: true,
   stagger: 30
