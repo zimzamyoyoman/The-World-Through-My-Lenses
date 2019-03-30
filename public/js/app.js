52ac53b65134d06677a730cbd5ec5bb245ada6be
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-03-30 18:27:24 +0530

    Added sidebar profile card

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var container = document.querySelector('#masonry');
   var masonry = new Masonry(container, {
-    columnWidth: 80,
+    columnWidth: 40,
     itemSelector: '.item',
     horizontalOrder: true,
     stagger: 30
